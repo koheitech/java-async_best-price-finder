909bf77fe3e533aad5106232767eae411e1284bf
koheitech <koheitechigh@gmail.com>
2022-11-30 12:08:49 +0200

    randomize the site fetching delay

--- src/FlightService.java
+++ src/FlightService.java
@@ -14,23 +14,23 @@ public class FlightService {
 
     public CompletableFuture<Quote> getQuote(String site) {
         return CompletableFuture.supplyAsync(() -> {
+            var random = new Random();
 
             System.out.println("Getting a quote from " + site);
 
             // sleep the current thread for 3 sec in order to simulate the remote process
             // that fetching price of the Quote remotely
-            sleepFor(3000);
+            sleepFor(1000 + random.nextInt(2000));
 
-            var random = new Random();
             double price = 100 + random.nextInt(10);
 
             return new Quote(site, price);
         });
     }
 
-    private void sleepFor(int millisec) {
+    private void sleepFor(int delay) {
         try {
-            Thread.sleep(millisec);
+            Thread.sleep(delay);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }

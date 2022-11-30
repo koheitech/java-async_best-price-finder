d120a5eeb252185dad039170d37c610642ca2904
koheitech <koheitechigh@gmail.com>
2022-11-30 12:13:15 +0200

    retrieve the quotes fetching time

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,25 @@
+import java.time.Duration;
+import java.time.LocalTime;
+import java.util.concurrent.CompletableFuture;
 import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
+        var start = LocalTime.now();
         var service = new FlightService();
-        service
+        var futures = service
                 .getQuotes()
                 .map(future -> future.thenAccept(System.out::println))
                 .collect(Collectors.toList());
+
+        CompletableFuture
+                .allOf(futures.toArray(new CompletableFuture[0]))
+                .thenRun(() -> {
+                    var end = LocalTime.now();
+                    var duration = Duration.between(start, end);
+                    System.out.println("Retrieved all quotes in " + duration.toMillis() + " milli seconds.");
+                });
+
 
         // sleep thread to output the result in the console
         try {
